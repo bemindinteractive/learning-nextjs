5414f7f13f2119f80f5a855209860b8943bdbbcc
joshuaudeh <91254045+joshuaudeh@users.noreply.github.com>
2021-09-24 20:38:39 +0200

    Navigate between pages (#2)
    * Navigate between pages
    
    * Link component
    
    * Added title Metadata
    
    * css module

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
-      <h1>How are you doing</h1>
+      {/* <h1>How are you doing</h1>
       <p>nextjs</p>
-      
+      <h2>Caleb</h2> */}
+      <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
     </div>
   )
 }
