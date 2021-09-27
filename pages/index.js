7820ac4adb570b6920f3e0b0126e402949df2bee
whirmill <lorenzo@vidra.com>
2021-09-27 10:38:51 +0200

    removed unnecessary code (not yet necessary) and rewritten index.js and posts/first-post.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      {/* <h1>How are you doing</h1>
-      <p>nextjs</p>
-      <h2>Caleb</h2> */}
       <h1 className="title">
-        Read{' '}
+        Read{" "}
         <Link href="/posts/first-post">
           <a>this page!</a>
         </Link>
       </h1>
     </div>
-  )
+  );
 }
