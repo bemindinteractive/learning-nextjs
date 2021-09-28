f7a9b76e024652684e6ec4a8fd0baccb39529cb5
Joshua Udeh <joshua.udeh@bemind.me>
2021-09-28 12:15:29 +0200

    fixed the bugs

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,19 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
