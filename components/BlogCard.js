3e47d4741a4d1078ae0a615ff5de072a16f588d8
whirmill <lorenzo@vidra.com>
2021-09-30 11:45:40 +0200

    fixed image loading

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,17 +1,21 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function BLogCard({ blog }) {
-    const { title, slug, thumbnail } = blog.fields
+export default function BLogCard({ recipe }) {
+  //   const { title, slug, thumbnail } = recipe.fields;
 
-    return (
-        <div className="card">
-            <div className="featured">
-                <Image
-                    src={'https:' + thumbnail.fields.file.url}
-                    width={thumbnail.fields.file.details.image.width}
-                    height={thumbnail.fields.file.details.image.height}
-                />
-            </div>
-        </div>
-    )
+  console.log(recipe.fields.image.fields.file);
+
+  return (
+    <div className="card">
+      <div className="featured">
+        <Image
+          src={"https:" + recipe.fields.image.fields.file.url}
+          //   width={thumbnail.fields.file.details.image.width}
+          //   height={thumbnail.fields.file.details.image.height}
+          layout="fill"
+          alt="test"
+        />
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,69 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Date from '../components/date'
-import { createClient } from 'contentful'
-import { getSortedPostsData } from '../lib/posts'
+// import Head from "next/head";
+// import Layout, { siteTitle } from "../components/layout";
+// import utilStyles from "../styles/utils.module.css";
+// import Link from "next/link";
+// import Date from "../components/date";
+// import { getSortedPostsData } from "../lib/posts";
+import { createClient } from "contentful";
+import BLogCard from "../components/BlogCard";
+
+// export function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// export default function Home({ allPostsData }) {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+//         <h2 className={utilStyles.headingLg}>Blog</h2>
+//         <ul className={utilStyles.list}>
+//           {allPostsData.map(({ id, date, title }) => (
+//             <li className={utilStyles.listItem} key={id}>
+//               <Link href={`/posts/${id}`}>
+//                 <a>{title}</a>
+//               </Link>
+//               <br />
+//               <small className={utilStyles.lightText}>
+//                 <Date dateString={date} />
+//               </small>
+//             </li>
+//           ))}
+//         </ul>
+//       </section>
+//     </Layout>
+//   );
+// }
 
 export async function getStaticProps() {
-
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
+  });
 
-  const res = await client.getEntries({ content_type: 'blog'})
-  const allPostsData = getSortedPostsData()
+  const res = await client.getEntries({ content_type: "blog" });
+
   return {
     props: {
-      allPostsData,
-      blogs: res.items
-    }
-  }
+      blogs: res.items,
+    },
+  };
 }
 
-export default function Recipes({ blogs}) {
+export default function Recipes({ blogs }) {
   return (
     <div className="recipe-list">
-      {blogs.map(blog =>(
-        <BlogCard key={blog.sys.id} recipe={blog} />
+      {blogs.map((blog) => (
+        <BLogCard key={blog.sys.id} recipe={blog} />
       ))}
     </div>
-  )
-}
-
-
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+  );
 }
