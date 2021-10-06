60300b3e15fe79e729b1e5a752420986cb1e954f
whirmill <lorenzo@vidra.com>
2021-10-06 12:43:34 +0200

    starting from scratch

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,4 @@
-// import Head from "next/head";
-//import Layout from "../components/layout";
-// import utilStyles from "../styles/utils.module.css";
-//  import Link from "next/link";
-//  import Date from "../components/date";
-// import { getSortedPostsData } from "../lib/posts";
 import { createClient } from "contentful";
-import BLogCard from "../components/BlogCard";
-
-// export function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
-
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
-
-// export default function Home({ allPostsData }) {
-//   return (
-//     <Layout home>
-//       <Head>
-//         <title>{siteTitle}</title>
-//       </Head>
-//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-//         <h2 className={utilStyles.headingLg}>Blog</h2>
-//         <ul className={utilStyles.list}>
-//           {allPostsData.map(({ id, date, title }) => (
-//             <li className={utilStyles.listItem} key={id}>
-//               <Link href={`/posts/${id}`}>
-//                 <a>{title}</a>
-//               </Link>
-//               <br />
-//               <small className={utilStyles.lightText}>
-//                 <Date dateString={date} />
-//               </small>
-//             </li>
-//           ))}
-//         </ul>
-//       </section>
-//     </Layout>
-//   );
-// }
 
 export async function getStaticProps() {
   const client = createClient({
@@ -58,14 +15,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function BlogCards({ blogs }) {
-  return (
-    <>
-      <div className="recipe-list">
-        {blogs.map((blog) => (
-          <BLogCard key={blog.sys.id} blog={blog} />
-        ))}
-      </div>
-    </>
-  );
+export default function Home(props) {
+  console.log(props);
+  return <>Hello World</>;
 }
